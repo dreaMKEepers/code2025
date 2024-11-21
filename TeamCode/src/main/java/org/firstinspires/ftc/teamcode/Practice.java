fe3c4ce17bf7c97012343836d358140e8ebb263c
Ishu <oonay668@gmail.com>
2024-11-21 16:16:11 -0600

    Copied from 11.19 code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Practice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Practice.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -8,9 +9,11 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 
+
+
 @TeleOp
 public class Practice extends LinearOpMode {
-    
+
     @Override
     public void runOpMode() throws InterruptedException {
         // Declare our motors
@@ -20,108 +23,97 @@ public class Practice extends LinearOpMode {
         DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
         DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
 
-        /* 
-        Reverse the right side motors. This may be wrong for your setup.
-        If your robot moves backwards when commanded to go forwards,
-        reverse the left side instead.
-        See the note about this earlier on this page.
-        */
+
         frontRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         backRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        DcMotor actuator = hardwareMap.get(DcMotor.class, "actuator"); // change to liftDCMotor
+
+        DcMotor liftDcMotor = hardwareMap.get(DcMotor.class, "liftDcMotor");
         CRServo grabServoMotor = hardwareMap.crservo.get("grabServoMotor");
-        //TODO: add wrist CRServo motor 
-                
+        CRServo wristServoMotor = hardwareMap.crservo.get("wristServoMotor");
+
 
         waitForStart();
 
+
         telemetry.addData("start ", "");
         if (isStopRequested()) return;
-           
-        /* Test test test */
-
-        /* I hate goding*/
 
         while (opModeIsActive()) {
-            
-        
-            // telemetry.addData("ZeroPowerBehavior: ", actuator.getZeroPowerBehavior());
-        telemetry.addData("Curr Position: ", actuator.getCurrentPosition());
 
-            
+            telemetry.addData("Curr Position: ", liftDcMotor.getCurrentPosition());
+
             /* Drive Code */
-            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
-            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
-            double rx = gamepad1.right_stick_x;
 
-            /* 
-            Denominator is the largest motor power (absolute value) or 1
-            This ensures all the powers maintain the same ratio,
-            but only if at least one is out of the range [-1, 1]
-            */ 
-            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 3);
-            double frontLeftPower = (y + x + rx) / denominator;
-            double backLeftPower = (y - x + rx) / denominator;
-            double frontRightPower = (y - x - rx) / denominator;
-            double backRightPower = (y + x - rx) / denominator;
 
-            frontLeftMotor.setPower(frontLeftPower);
-            backLeftMotor.setPower(backLeftPower);
-            frontRightMotor.setPower(frontRightPower);
-            backRightMotor.setPower(backRightPower);
-            
-            /* Grab Servo Code */
-            // if (gamepad1.y) { // push block out 
-            //   grabServoMotor.setPower(1);
-            // } else if (gamepad1.a) { // grab block 
-            //     grabServoMotor.setPower(-1);
+            while (gamepad1.dpad_up){
+                frontRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+                frontRightMotor.setPower(1);
+
+
+                frontLeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+                frontLeftMotor.setPower(1);
+            }
+
+            while (gamepad1.dpad_down) {
+                frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+                frontRightMotor.setPower(1);
+
+
+                frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+                frontLeftMotor.setPower(1);
+            }
+
+
+            /* Grab Motor*/
+            while(gamepad2.y) {
+                grabServoMotor.setPower(1);
+                telemetry.addData("gamepad2.y is pressed", gamepad2.y);
+            }
+
+
+            while(gamepad2.a) {
+                grabServoMotor.setPower(-1);
+                telemetry.addData("gamepad2.a is pressed", gamepad2.a);
+            }
+
+
+            /* Lift Motor */
+            while (gamepad2.left_bumper){
+                liftDcMotor.setPower(1);
+                telemetry.addData("Current Position left", liftDcMotor.getCurrentPosition());
+            }
+            while (gamepad2.right_bumper) {
+                liftDcMotor.setPower(-1);
+                telemetry.addData("Current Position right", liftDcMotor.getCurrentPosition());
+            }
+
+
+
+
+            /* Wrist Motor */
+            // while (gamepad2.left_trigger){
+            //     wristServoMotor.setDirection(0);
+            //     telemetry.addData("wrist move left", liftDcMotor.getCurrentPosition());
             // }
-        
-        while(gamepad1.y) { 
-        grabServoMotor.setPower(1);
-        
-           telemetry.addData("gamepad1.y is pressed", gamepad1.y);
-        } 
-        
-        /* Lift Motor */
-        if (gamepad1.left_bumper){
-              actuator.setPower(1);
-             // actuator.setPosition(1); 
-             // actuator.setPosition(0); 
-           // actuator.setTargetPosition(1);
-            telemetry.addData("Current Position left", actuator.getCurrentPosition());
-        } else if(gamepad1.right_bumper) {
-                actuator.setPower(-1);
-             //   actuator.setPosition(0.5); 
-        telemetry.addData("Current Position right", actuator.getCurrentPosition());
-
-        } else {
-            actuator.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            int currentPosition = actuator.getCurrentPosition();
-             actuator.setPower(0);
+            // while (gamepad2.right_trigger) {
+            //     wristServoMotor.setDirection(1);
+            //   telemetry.addData("wrist move right", actuator.getCurrentPosition());
+            // }
+            liftDcMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            int currentPosition = liftDcMotor.getCurrentPosition();
+            liftDcMotor.setPower(0);
             // actuator.setTargetPosition();
             // actuator.setPosition(0.5);
-            // public static final DcMotor.ZeroPowerBehavior BRAKE 
+            // public static final DcMotor.ZeroPowerBehavior BRAKE
             // look into while loops
-            telemetry.addData("Current Position ", actuator.getCurrentPosition());
-            } 
-            
-        //while(actuator.getPower() == 0) {
-            
-            //actuator.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        //telemetry.addData("Brake is on", "");
-            
-           // setZeroPowerBehavior BRAKE;
-         }
-        
-        
-     
-        telemetry.update();
-        
-        } // end opModeActive loop 
-        
-    }
+            telemetry.addData("Current Position ", liftDcMotor.getCurrentPosition());
+        }
 
+        telemetry.update();
+
+    } // end opModeActive loop
+
+}
